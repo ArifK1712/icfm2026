618990e1d26aed3510ffd2a923fb408739f4ca31
Arif Khan <arif@letuscode.com>
2026-06-04 16:45:59 +0530

    Marquee changed

--- src/components/home/Marquee.jsx
+++ src/components/home/Marquee.jsx
@@ -3,7 +3,7 @@ function Marquee() {
     'Important Dates: Abstract Submission closes on 30 July 2026 • Early Bird Registration ends on 11 August 2026.'
 
   return (
-    <section className="overflow-hidden bg-teal-500 text-white rotate-1">
+    <section className="overflow-hidden text-white">
       <div className="flex w-max animate-marquee">
         {[...Array(8)].map((_, index) => (
           <div
@@ -11,8 +11,8 @@ function Marquee() {
             className="
               flex shrink-0 items-center
               whitespace-nowrap py-4
-              text-xl font-black uppercase
-              tracking-[1px]
+              text-5xl font-black uppercase text-teal-400
+              tracking-[1px] 
             "
           >
             <span>{text}</span>
